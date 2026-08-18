22adf834e56c0be52dba6b2728f7aa01032a8709
DelightLane <hano881112@hanmir.com>
2026-08-18 10:39:50 +0900

    입력 테스트 기능 추가

---
+++ app/src/main/java/me/blog/hgl1002/openwnn/view/TestInputActivity.java
@@ -0,0 +1,54 @@
+package me.blog.hgl1002.openwnn.view;
+
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.OnApplyWindowInsetsListener;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import me.blog.hgl1002.openwnn.R;
+
+public class TestInputActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_input);
+        if(getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle(R.string.preference_test_input_menu);
+        }
+
+        EditText singleLineInput = findViewById(R.id.test_input_single);
+        EditText multiLineInput = findViewById(R.id.test_input_multi);
+
+        findViewById(R.id.select_ime_button).setOnClickListener(v -> {
+            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+            if(imm != null) {
+                imm.showInputMethodPicker();
+            }
+        });
+
+        findViewById(R.id.clear_button).setOnClickListener(v -> {
+            singleLineInput.setText("");
+            multiLineInput.setText("");
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scroll_view), (OnApplyWindowInsetsListener) (view, windowInsets) -> {
+            Insets insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars());
+            view.setPadding(insets.left, insets.top, insets.right, insets.bottom);
+            return WindowInsetsCompat.CONSUMED;
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+}
