317b1ff2279875ee3b094e329a1333c7cc6619ba
DelightLane <hano881112@hanmir.com>
2026-08-18 14:27:11 +0900

    라이센스에 맞춰 병기

--- app/src/main/java/com/delightlane/keyboard/InputViewManager.java
+++ app/src/main/java/com/delightlane/keyboard/InputViewManager.java
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * This file has been modified from its original form as part of the
+ * openwnn-korean project (https://github.com/Lee0701/openwnn-korean)
+ * and this fork (delight-keyboard).
  */
 
 package com.delightlane.keyboard;
